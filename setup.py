6c341b8dca56a0bb313d6aa001a7c7e95ea4cc0e
Khizer Younas <m.khizeryounas@gmail.com>
2020-01-22 16:57:41 +0500

    fix: added attr alter

--- setup.py
+++ setup.py
@@ -255,7 +255,7 @@ class RunTests(ExtendedTestCommand):
 ###############################
 
 setup(
-    name='spyne',
+    name='spyne-shopdev',
     packages=find_packages(),
 
     version=VERSION,
